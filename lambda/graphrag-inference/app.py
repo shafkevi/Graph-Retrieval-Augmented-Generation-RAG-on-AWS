555691d9c383a8733f7b84bd50fb5e09fcc183e7
Aditya Chaphekar <adichap@amazon.com>
2025-06-10 15:04:40 +0100

    fixed inference lambda

--- lambda/graphrag-inference/app.py
+++ lambda/graphrag-inference/app.py
@@ -205,13 +205,13 @@ def lambda_handler(event, context):
             
             return {
                 'statusCode': 200,
-                'headers': {
-                    'Content-Type': 'text/plain',
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
-                    'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
-                    'Transfer-Encoding': 'chunked'
-                },
+                #'headers': {
+                 #   'Content-Type': 'text/plain',
+                  #  'Access-Control-Allow-Origin': '*',
+                  #  'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                   # 'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
+                   # 'Transfer-Encoding': 'chunked'
+               # },
                 'body': full_response
             }
             
